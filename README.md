618c1267f8480f9f3146e45cb0b1253309e2261c
xiongchengbin <xiongchengbin@meituan.com>
2018-09-02 01:25:24 +0800

    归类文件

--- README.md
+++ README.md
@@ -1,20 +1,2 @@
-### 2018-2-23<br />
-+ [单页应用的数据流方案探索](https://zhuanlan.zhihu.com/p/26426054)<br />
-
-### 2018-2-7<br />
-+ [Service Worker 全面进阶 | Harries Blog™](http://www.liuhaihua.cn/archives/454811.html)<br />
-
-### 2018-1-19<br />
-+ [菜鸟网络前端全栈化之路_前端之巅_传送门](http://chuansong.me/n/2146235952511)<br />
-
-### 2018-1-15<br />
-+ [轻推转型之门：Ant Design Pro 在企业级产品的实践（附演讲视频）](https://zhuanlan.zhihu.com/p/32771546)<br />
-+ [从前端技术到体验科技（附演讲视频）](https://zhuanlan.zhihu.com/p/32782686)<br />
-
-### 2018-1-13<br />
-+ [配色工具-专业的配色工具大全-千图网www.58pic.com](http://www.58pic.com/peise/)<br />
-
-### 2018-1-2<br />
-+ [HTTP/2 PUSH(推送)与HTTP Preload(预加载)大比拼| Dexecure - 众成翻译](http://www.zcfy.cc/article/http-2-push-vs-http-preload-dexecure-4722.html?t=new)<br />
-+ [百度外卖如何做到前端开发配置化-博客-云栖社区-阿里云](https://yq.aliyun.com/articles/112472)<br />
-+ [美团外卖前端可视化界面组装平台 —— 乐高 -](https://tech.meituan.com/waimai-lego.html)<br />
+### 2018-9-2 <br/>
++ [JavaScript Module Pattern: In-Depth](http://www.adequatelygood.com/JavaScript-Module-Pattern-In-Depth.html) <br/>
--- bookmark.js
+++ bookmark.js
@@ -12,23 +12,20 @@ http.createServer((req, res) => {
 	//console.dir(urlData);
 	let queryObj = querystring.parse(urlData.query);
 	console.dir(queryObj);
-	queryObj.title && queryObj.url && writeToMarKdownDoc(queryObj);
-	res.writeHead(200, {"Content-Type": "application/json"});
-	res.end(JSON.stringify({
-		code: 0,
-		msg : 'success'
-	}));
+	queryObj.title && queryObj.url && writeToMarkdown(queryObj);
+	res.writeHead(204, {"Content-Type": "image/jpeg"});
+	res.end();
 }).listen(3117);
 
 
-function changeFilename () {
+function getFilename () {
 	let fileName = 'README.md';
 	let name = '';
 	let date = new Date();
 	if (date.getMonth() + 1 !== currentMonth) {
 		name = date.getFullYear() + '-' + date.getMonth() + '.md';
 		try {
-			let state = fs.state(name);
+			let state = fs.statSync(name);
 			!state.isFile() && fs.renameSync(fileName, name);
 		}
 		catch (e){
@@ -42,8 +39,7 @@ function changeFilename () {
 function isNewFile(filename) {
 	try {
 		let fileState = fs.statSync(filename);
-		let result = fileState.isFile();
-		return result;
+		return fileState.isFile();
 	}
 	catch(e) {
 		//console.error(e);
@@ -51,7 +47,7 @@ function isNewFile(filename) {
 	}
 }
 
-function writeToFile(filename, data, title) {
+function writeToFile(filename, data) {
 	fs.writeFile(filename, data, (err) => {
 		if (err) console.err(err);
 		console.log('saved ' + filename);
@@ -59,14 +55,22 @@ function writeToFile(filename, data, title) {
 	});
 }
 
-function writeToMarKdownDoc(obj) {
-	let fileName = changeFilename();
+function pushContent(data, dateLine, queryData) {
+  const date = new Date();
+  const d = decodeURIComponent;
+  dateLine = dateLine || `### ${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} <br/>`;
+  data.push(dateLine);
+  data.push(`+ [${d(queryData.title)}](${queryData.url}) <br/>`);
+  queryData.desc && data.push(`    ${d(queryData.desc)} <br/>`)
+}
+
+function writeToMarkdown(obj) {
+	let fileName = getFilename();
 	let data = [];
 	let inserted = false;
 	const date = new Date();
 	if (!isNewFile(fileName)) {
-		data.push(`### ${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}<br />`)
-		data.push(`+ [${obj.title}](${obj.url})<br />\n`);
+	  pushContent(data, false, obj);
 		writeToFile(fileName, data.join('\n'), obj.title);
 		return;
 	}
@@ -76,35 +80,27 @@ function writeToMarKdownDoc(obj) {
 
 	rl.on('line', (line) => {
 		if (!inserted && line.startsWith('###')) {
-
+      inserted = true;
 			let date_str = line.match(/\d{4}-\d{1,2}-\d{1,2}/)[0];
 			if (date_str && (new Date(date_str).getDate() === date.getDate())) {
-				data.push(line);
-				data.push(`+ [${obj.title}](${obj.url})<br />`);
+			  pushContent(data, line, obj);
+			  return;
 			}
-			else {
-				data.push(`### ${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}<br />`)
-				data.push(`+ [${obj.title}](${obj.url})<br />\n`);
-				data.push(line);
-			}
-			inserted = true;
+			pushContent(data, false, obj);
 		}
-		else {
-			data.push(line);
-		}
+		data.push(line);
 	});
 	rl.on('close', () => {
 		if (!inserted) { //新建的文件
-			data.push(`### ${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}<br />`)
-			data.push(`+ [${obj.title}](${obj.url})<br />\n`);
+			pushContent(data, false, obj);
 		}
-		writeToFile(fileName, data.join('\n'), obj.title);
+		writeToFile(fileName, data.join('\n'));
 	});
 }
 
 function pushToGit(title) {
-	var title = title || '';
-	var cmds = [
+  title = title || '';
+	const cmds = [
 	    ['git', ['add', '.']],
 	    ['git', ['commit', '-am', `"add ${title}"`]],
 	    ['git', ['pull', '--rebase']],
@@ -119,6 +115,6 @@ function pushToGit(title) {
 setInterval(function(){
 	console.log('push to github');
 	pushToGit(new Date().toString());
-}, 1000 * 60 * 60 * 2);
+}, 1000 * 60 * 60 * 3);
 
 console.log('server start at 3117');
